c375e13079a5864fe458a10cc9aa7a23a13e27f0
HollowRaine <EnchantedCaptainDave@gmail.com>
2025-04-23 13:08:20 -0500

    PLEASE WORK

--- GameDevProject/Assets/Alive/Player/Player.cs
+++ GameDevProject/Assets/Alive/Player/Player.cs
@@ -1,31 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Net.NetworkInformation;
-using Unity.Collections;
-using UnityEditor.Callbacks;
-using UnityEngine;
 
-  /* 
-       1. Find a way to input stats into movment
+
+//   /* 
+//        1. Find a way to input stats into movment
    
    
-    */
-public class Player : Character
-{
+//     */
+// public class Player : Character
+// {
 
 
-    void Start()
-    {
+//     void Start()
+//     {
     
-      double x =  statistics.Get("HI");
-      print(x + "s");
+//       double x =  statistics.Get("HI");
+//       print(x + "s");
 
-    }
+//     }
 
-    // Update is called once per frame
-    void Update()
-    {
+//     // Update is called once per frame
+//     void Update()
+//     {
        
-    }
-}
+//     }
+// }
