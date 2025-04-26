9a2be5ae1da58339d1b7f4526e7dbe5521c74f2e
HollowRaine <EnchantedCaptainDave@gmail.com>
2025-04-25 19:14:33 -0500

    Updated movement values. Generalized movement a bit. Added comments
    Organized the code. Deleted old code. Comments on everything. Better values for everything HOWEVER still kinda clunky

--- GameDevProject/Assets/Alive/Movement/Movement.cs
+++ GameDevProject/Assets/Alive/Movement/Movement.cs
@@ -1,26 +1,38 @@
-using System;
-using Unity.Mathematics;
 using UnityEngine;
 
-//1. Need to create Constriants on inputs
+// 4/25/2025(rain) Collider2D should in theory be the generic thing. However you cannot reference the collider's size if you use collider2d. If you use BoxCollider2D
+// you are able to access the size of the collider. We need to access it because of grounded check depends on how far the entity is away from the ground. Using the
+// collider's y size is the correct size for the raycast. I believe there is a work around but ima work on something else
 
 
 public class Movement : MonoBehaviour
 {
+    [Header("Components")]
+    [SerializeField] private Statistics playerStats;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private CameraManager camMan;
+    [SerializeField] private BoxCollider2D cd;
+    [SerializeField] private LayerMask platLayer;
 
-    [SerializeField]private Statistics playerStats;
-    [SerializeField]private Rigidbody2D rb;
-    [SerializeField]private CameraManager CamMan;
-    [SerializeField]private bool grounded;
-    [SerializeField]private float hori;
-    [SerializeField]private Vector2 moveDirection;
-    [SerializeField]private float moveSpeed = 100f;
-    [SerializeField]private float jumpForce = 10f;
-    [SerializeField]private bool readyToJump;
-    [SerializeField]private float jumpCooldown = .25f;
-    [SerializeField]private LayerMask platLayer;
-    [SerializeField]private float groundDrag = 5f;
-    [SerializeField]private float airMultiplier = .2f;
+    [Header("Ground Movement")]
+    [SerializeField] private float moveSpeed = 100f;
+    [SerializeField] private float groundDrag = 5f;
+
+
+    [Header("Inputs")]
+    [SerializeField] private float hori;
+    [SerializeField] private Vector2 moveDirection;
+    [SerializeField] private bool kSpace;
+
+    [Header("Jump")]
+    [SerializeField] private float jumpForce = 10f;
+    [SerializeField] private float airMultiplier = .2f;
+    [SerializeField] private float jumpCooldown = .25f;
+    [SerializeField] private bool readyToJump;
+    [SerializeField] private bool grounded;
+
+
+
     // public Movement(Statistics stats)
     // {
     //     playerStats = stats;
@@ -29,22 +41,24 @@ public class Movement : MonoBehaviour
     public void Awake()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
-        CamMan = GetComponent<CameraManager>();
-        
+        camMan = GetComponent<CameraManager>();
+        cd = GetComponent<BoxCollider2D>();
     }
 
     void Start()
     {
         readyToJump = true;
+
     }
 
     public void Update()
     {
-        grounded = Physics2D.Raycast(transform.position, Vector2.down, 0.7431644f, platLayer); // Checks if player is on ground. Calculates Drag
-        print(grounded);
+        grounded = Physics2D.Raycast(transform.position, Vector2.down, cd.size.y, platLayer); // Checks if player is on ground.
+        print("X" + rb.velocity.x);
+        print("Y" + rb.velocity.y);
         MyInput();
         SpeedControl();
-        rb.drag = grounded ? groundDrag : 2f;
+        rb.drag = grounded ? groundDrag : 4f; //Calculates drag based on if entity is in air or not
 
         // if (hori > 0 || hori < 0) CamMan.Turn();
     }
@@ -53,18 +67,24 @@ public class Movement : MonoBehaviour
         Move();
     }
 
+    // Gets input from user. Left,Right movement and Jump.
     public void MyInput()
     {
+        // Keyboard Inputs
         hori = Input.GetAxis("Horizontal");
-        if (Input.GetKey(KeyCode.Space) && readyToJump && grounded)
+        kSpace = Input.GetKey(KeyCode.Space);
+
+        // Checks if player can jump. Jumps if yes.
+        if (kSpace && readyToJump && grounded)
         {
             readyToJump = false;
             Jump();
-            Invoke(nameof(ResetJump), jumpCooldown);
+            Invoke(nameof(ResetJump), jumpCooldown); // Jump Cooldown.
         }
     }
 
     // Makes sure speed does not exceed limit
+    // 4/25/2025 I don't think this works as intended currently
     public void SpeedControl()
     {
         Vector2 flatVel = new Vector2(rb.velocity.x, 0f);
@@ -76,78 +96,26 @@ public class Movement : MonoBehaviour
         }
 
     }
-    // Calculates the x direction of player on ground and air. Does not allow player the jump.
+    // Calculates the x velocity of player on ground and air.
     public void Move()
     {
-        moveDirection = Vector2.right * hori;
+        moveDirection = Vector2.right * hori; // Uses user input to find what direction player is moving
 
-        if (grounded) rb.AddForce(moveDirection.normalized * moveSpeed, ForceMode2D.Force);
-        else if (!grounded) rb.AddForce(moveDirection.normalized * moveSpeed * airMultiplier, ForceMode2D.Force);
+        if (grounded) rb.AddForce(moveDirection.normalized * moveSpeed, ForceMode2D.Force); // Speed while grounded
+        else if (!grounded) rb.AddForce(moveDirection.normalized * moveSpeed * airMultiplier, ForceMode2D.Force); // Speed while in Air(Maintains the momentum before in air)
     }
 
+    // Calculates entity jump.
     public void Jump()
     {
-        rb.velocity = new Vector2(rb.velocity.x, 0f);
+        rb.velocity = new Vector2(rb.velocity.x, 0f); // Keeps velocity in x speed. Resets velocity in y speed.
         rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
     }
 
+    // Resets jump. Allows entity to jump when true.
     public void ResetJump()
     {
         readyToJump = true;
     }
 
-    // public void Move(Vector2 input)
-    // {
-
-    //     if (math.abs(rb.velocity.x) < 5)
-    //     {
-    //         rb.velocity += input;
-    //         //rb.AddForce( input, ForceMode2D.Impulse);
-    //         // ,         
-    //     }
-    //     if (Input.GetKeyDown(KeyCode.Space))
-    //     {
-    //         Vector2 dir = new Vector2(0, 40);
-    //         if (!isFalling)
-    //         {
-    //             rb.AddForce(dir, ForceMode2D.Impulse);
-
-    //         }
-    //     }
-    // }
-
-
-
-    // private void OnCollisionEnter2D(Collision2D other)
-    // {
-    //     LayerMask Platforms = LayerMask.GetMask("Platforms");
-    //     RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, Mathf.Infinity, Platforms);
-    //     // Debug.DrawRay(transform.position, Vector3.down, Color.red);
-    //     if (other.gameObject.CompareTag("Cave") && hit.distance <= .5)
-    //     {
-    //         isFalling = false;
-
-    //     }
-    //     print(hit.distance);
-    // }
-
-    // private void OnCollisionExit2D(Collision2D other)
-    // {
-    //     if (other.gameObject.CompareTag("Cave"))
-    //     {
-    //         isFalling = true;
-    //     }
-    //     print(isFalling);
-    // }
-    /* 
-    * Get inputs 
-            movment 
-
-        Add constraitnes based on the stats of the character
-        change the position and rotation of the character based on stats
-
-
-
-    */
 }
-
