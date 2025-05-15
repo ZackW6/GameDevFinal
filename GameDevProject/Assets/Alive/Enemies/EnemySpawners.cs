afaaa2d525cd617ec42e08bff256d2b4ba6fbb4d
ZackW6 <154006365+ZackW6@users.noreply.github.com>
2025-05-15 13:12:42 -0500

    pleaseWork

---
+++ GameDevProject/Assets/Alive/Enemies/EnemySpawners.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawners : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public float frequency;
+    public float timer;
+    void Start()
+    {
+        timer = frequency;
+    }
+
+
+ 
+    void Update()
+    {
+        if(timer <= 0){
+            Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+            timer = frequency;
+        }
+        timer -= Time.deltaTime;
+    }
+}
--- GameDevProject/Assets/AudioManager.cs
+++ GameDevProject/Assets/AudioManager.cs
@@ -1,50 +1,77 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 
 public class AudioManager : MonoBehaviour
 {
   [Header(" ------- Audio Source ----------")]
   [SerializeField] AudioSource musicSource;
 
+
   public double musicDuration;
   public double goalTime;
+  public AudioSource[] _audioSource;
+  public int audioToggle;
   public AudioClip currentclip;
-  public AudioSource audioSource;
-  public AudioClip background;
+
+
+  // public AudioClip background;
+
 
   private void Start()
   {
-    musicSource.clip = background;
-    musicSource.Play();
+    OnPlayMusic();
   }
 
-  // private void OnPlayMusic()
-  // {
-  //   goalTime = AudioSettings.dspTime + 0.5;
 
-  //   audioSource.clip = currentclip;
-  //   audioSource.PlayScheduled(goalTime);
+  private void OnPlayMusic()
+  {
+    goalTime = AudioSettings.dspTime + 0.5;
 
-  //   musicDuration = (double)currentclip.samples / currentclip.frequency;
-  //   goalTime = goalTime + musicDuration;
-  // }
 
-  // private void Update()
-  // {
-  //   {
-  //     if (AudioSettings.dspTime > goalTime - 1)
-  //     {
-  //       audioSource.clip = currentclip;
-  //       audioSource.PlayScheduled(goalTime);
+     _audioSource[audioToggle].clip = currentclip;
+     _audioSource[audioToggle].PlayScheduled(goalTime);
 
-  //       musicDuration = (double)currentclip.samples / currentclip.frequency;
-  //       goalTime = AudioSettings.dspTime  + musicDuration;
-  //     }
-  //   }
-  // }
 
-  // private void playScheduledClip(){
-  //   /* https://www.youtube.com/watch?v=3yKcrig3bU0 */
-  // }
+    musicDuration = (double)currentclip.samples / currentclip.frequency;
+    goalTime = goalTime + musicDuration;
+  }
+
+
+  private void Update()
+  {
+    if (AudioSettings.dspTime > goalTime - 1){
+      playScheduledClip();
+    }
+    // {
+    //   if (AudioSettings.dspTime > goalTime - 1)
+    //   {
+    //     audioSource.clip = currentclip;
+    //     audioSource.PlayScheduled(goalTime);
+
+
+    //     musicDuration = (double)currentclip.samples / currentclip.frequency;
+    //     goalTime = AudioSettings.dspTime  + musicDuration;
+    //   }
+    // }
+  }
+
+
+  private void playScheduledClip(){
+    _audioSource[audioToggle].clip = currentclip;
+    _audioSource[audioToggle].PlayScheduled(goalTime);
+
+
+  musicDuration = (double)currentclip.samples / currentclip.frequency;
+        goalTime =goalTime  + musicDuration;
+    audioToggle = 1-audioToggle;
+  }
+
+
+  public void setCurrentClip (AudioClip Clip){
+    Clip = currentclip;
+  }
+ 
 }
